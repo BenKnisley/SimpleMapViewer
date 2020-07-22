aad1de70bf58069ad26c0a34cf24b922d9ea9b14
BenKnisley <benknisley@gmail.com>
2020-07-22 19:50:13 -0400

    Changed v to 0.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name = "MapViewer",
-    version = "0.0.0",
+    version = "0.0.1",
     author = "Ben Knisley",
     author_email = "benknisley@gmail.com",
     description = ("A module for rendering maps."),
