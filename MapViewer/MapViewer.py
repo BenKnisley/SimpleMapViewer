a47d2ca33add3cae3020bcd3ee77abdfc04e845a
BenKnisley <benknisley@gmail.com>
2020-11-17 19:56:14 -0500

    Added TileLayer tool

--- MapViewer/MapViewer.py
+++ MapViewer/MapViewer.py
@@ -84,10 +84,117 @@ class FeatureStyle:
 
 
 selected_style = FeatureStyle()
-#selected_style.set_color('yellow')
+selected_style.set_color('yellow')
 
 
-class SelectTool(GObject.GObject):
+
+class MapViewerApplication(Gtk.Application):
+    """ The root GTK object for the application. Opens MainWindow. """
+    def __init__(self):
+        """ Opens MainWindow and connects signals & slots. """
+        ## Set ID and flags, initialize Gtk Application parent.
+        app_id="apps.test.MapViewer.id" + str(randint(1,1000))
+        flags=Gio.ApplicationFlags.FLAGS_NONE
+        Gtk.Application.__init__(self, application_id=app_id, flags=flags)
+
+        ## Initialize self object.
+        self.window = MainWindow()
+
+        ## Connect self activate signal, with self on_activate slot
+        self.connect("activate", self._on_activate)
+
+    def _on_activate(self, caller):
+        self.window.show_all()
+        self.add_window(self.window)
+
+
+class LayerView(Gtk.TreeView):
+    def __init__(self, parent_map):
+        ## Init as a TreeView obj
+        Gtk.TreeView.__init__(self)
+        self.parent_map = parent_map
+
+        ## Make item selected on single click
+        self.set_activate_on_single_click(True)
+
+        ## Setup a ListStore as a data model
+        self.store = Gtk.ListStore(object, str) ## MapLayer, layer name
+        self.set_model(self.store)
+
+        self.set_reorderable(True)
+
+        ## Setup a single column as a model
+        rendererText = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("                                   Map Layers                                   ", rendererText, text=1)
+        self.append_column(column)
+
+        ## Connect selection changes with selection_changed_slot
+        selection_watcher = self.get_selection()
+        selection_watcher.connect("changed", self.selection_changed_slot)
+        #self.store.connect("row-inserted", self.ping1)
+        self.store.connect("row-deleted", self.push_model_to_layer_list)
+    
+    def push_model_to_layer_list(self, treemodel, path):
+        """ Sets current order of items to maps layer list """
+        ## 
+        for i, iter in enumerate(treemodel):
+            self.parent_map._layer_list[i] = iter[0]
+
+        self.parent_map.call_rerender(self)
+        self.parent_map.call_redraw(self)
+    
+        #for layer in self.parent_map._layer_list:
+
+    #def selection_changed_slot(self, treeview, index, view_column):
+    def selection_changed_slot(self, caller):
+        """
+        Updated active layer index when called
+        """
+        ## Get model and treepath of selected row
+        selection_watcher = self.get_selection()
+        model, treepath = selection_watcher.get_selected_rows()
+
+        ## Get layer from model and treepath
+        layer = model[treepath][0]
+
+        ## Get Index of layer in map layer list
+        layer_index = self.parent_map._layer_list.index(layer)
+
+        ## Set active layer index to selected layer index
+        self.parent_map.active_layer_index = layer_index
+
+        
+        
+        
+        #tree_row = treeview.get_model()[index]
+        #self.parent_map.active_layer_index = self.parent_map._layer_list.index(tree_row[1])
+        #view_column
+
+    def layer_added_slot(self, caller, layer):
+        """
+        Adds a given MapLayer to the layer list
+        """
+        ## Add layer name and layer to model
+        self.store.append([layer, layer.name])
+
+        ## Set selection new layer
+        index = len(self.store) - 1
+        path = Gtk.TreePath.new_from_indices([index])
+        self.set_cursor_on_cell(path, None, None, False)
+
+
+    def layer_removed_slot(self, caller, layer):
+        """
+        Removes a given MapLayer to the layer list
+        """
+        #!! THIS IS A HACK AND I SHOULD FIND A BETTER WAY
+        for r in self.store:
+            if r[0] == layer:
+                self.store.remove(r.iter)
+
+
+
+class SelectTool(GObject.GObject): ## This serves as a base tool to copy from
     __gsignals__ = {
     "features-selected": (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (object,)),
     "selection-cleared": (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
@@ -223,101 +330,187 @@ class SelectTool(GObject.GObject):
                 self.parent.renderer.draw_polygon(cr, f.geometry.structure, x_values, y_values, selected_style)
 
 
-
-class MapViewerApplication(Gtk.Application):
-    """ The root GTK object for the application. Opens MainWindow. """
-    def __init__(self):
-        """ Opens MainWindow and connects signals & slots. """
-        ## Set ID and flags, initialize Gtk Application parent.
-        app_id="apps.test.MapViewer.id" + str(randint(1,1000))
-        flags=Gio.ApplicationFlags.FLAGS_NONE
-        Gtk.Application.__init__(self, application_id=app_id, flags=flags)
-
-        ## Initialize self object.
-        self.window = MainWindow()
-
-        ## Connect self activate signal, with self on_activate slot
-        self.connect("activate", self._on_activate)
-
-    def _on_activate(self, caller):
-        self.window.show_all()
-        self.add_window(self.window)
-
-
-class LayerView(Gtk.TreeView):
-    def __init__(self, parent_map):
-        ## Init as a TreeView obj
-        Gtk.TreeView.__init__(self)
-        self.parent_map = parent_map
-
-        ## Make item selected on single click
-        self.set_activate_on_single_click(True)
-
-        ## Setup a ListStore as a data model
-        self.store = Gtk.ListStore(object, str) ## MapLayer, layer name
-        self.set_model(self.store)
-
-        self.set_reorderable(True)
-
-        ## Setup a single column as a model
-        rendererText = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("                                   Map Layers                                   ", rendererText, text=1)
-        self.append_column(column)
-
-        ## Connect selection changes with selection_changed_slot
-        selection_watcher = self.get_selection()
-        selection_watcher.connect("changed", self.selection_changed_slot)
-        #self.store.connect("row-inserted", self.ping1)
-        self.store.connect("row-deleted", self.push_model_to_layer_list)
+class IdentifyTool(GObject.GObject):
+    __gsignals__ = {
+        "features-selected": (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (object,)),
+        "selection-cleared": (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
+    }
     
-    def push_model_to_layer_list(self, treemodel, path):
-        """ Sets current order of items to maps layer list """
-        ## 
-        for i, iter in enumerate(treemodel):
-            self.parent_map._layer_list[i] = iter[0]
+    def __init__(self, window):
+        GObject.GObject.__init__(self)
+        self.parent = None
+        self.connection_list = []
+        self.select_box_active = False
+        self.selected = []
 
-        self.parent_map.call_rerender(self)
-        self.parent_map.call_redraw(self)
+        self.window = window
+
+        self.display = IdentifyDisplay()
+        
+        self.connect("features-selected", self.ping)
+        self.connect("selection-cleared", self.ping2)
     
-        #for layer in self.parent_map._layer_list:
+    def activate(self, parent_map):
+        ## Set parent object
+        self.parent = parent_map
 
-    #def selection_changed_slot(self, treeview, index, view_column):
-    def selection_changed_slot(self, caller):
-        """
-        Updated active layer index when called
-        """
-        ## Get model and treepath of selected row
-        selection_watcher = self.get_selection()
-        model, treepath = selection_watcher.get_selected_rows()
+        ## Setup connection, keeping the reference to each in connection_list
+        self.connection_list.append( self.parent.connect("double-click", self.select_at_click) )
+        self.connection_list.append( self.parent.connect("middle-click", self.deselect_click) )
+        self.connection_list.append( self.parent.connect("middle-drag-start", self.select_box_start) )
+        self.connection_list.append( self.parent.connect("middle-drag-update", self.select_box_update) )
+        self.connection_list.append( self.parent.connect("middle-drag-end", self.select_box_end) )
 
-        ## Get layer from model and treepath
-        layer = model[treepath][0]
+        ##
+        self.window.sidebar.pack_start(self.display, True, True, 0)
+        self.window.sidebar.show_all()
 
-        ## Get Index of layer in map layer list
-        layer_index = self.parent_map._layer_list.index(layer)
+    def deactivate(self):
+        ## Reset tool vars
+        self.select_box_active = False
+        self.selected = []
 
-        ## Set active layer index to selected layer index
-        self.parent_map.active_layer_index = layer_index
-
+        ## Disconnect all connection
+        for connect_id in self.connection_list:
+            self.parent.disconnect(connect_id)
         
-        
-        
-        #tree_row = treeview.get_model()[index]
-        #self.parent_map.active_layer_index = self.parent_map._layer_list.index(tree_row[1])
-        #view_column
+        ## Clear Connection list
+        self.connection_list = []
+
+        ## Remove parent
+        self.parent = None
+
+        self.display.clear_display()
+        self.window.sidebar.remove(self.display)
+        self.window.sidebar.show_all()
+    
+        def select_at_click(self, caller, x, y):
+            """ """
+            self.deselect_all(self)
+
+            ## Get current active layer
+            layer = self.parent.get_layer( self.parent.active_layer_index )
+            if isinstance(layer, VectorLayer):
+                ## Get proj coords of click
+                proj_x, proj_y = self.parent.pix2proj(x, self.parent.height - y)
+
+                ## Get features at point
+                selected_features = layer.point_select(proj_x, proj_y)
+
+                ## Add all selected features to selected_features list
+                for feature in selected_features:
+                    if feature not in self.selected:
+                        self.selected.append(feature)
+
+            ## Emit selected_features 
+            self.emit("features-selected", self.selected)
+
+            ## Redraw widget with selected features highlighted
+            self.parent.call_redraw(self)
+    
+    def select_at_click(self, caller, x, y):
+        """ """
+        self.deselect_all(self)
+
+        ## Get current active layer
+        layer = self.parent.get_layer( self.parent.active_layer_index )
+        if isinstance(layer, VectorLayer):
+            ## Get proj coords of click
+            proj_x, proj_y = self.parent.pix2proj(x, self.parent.height - y)
+
+            ## Get features at point
+            selected_features = layer.point_select(proj_x, proj_y)
+
+            ## Add all selected features to selected_features list
+            for feature in selected_features:
+                if feature not in self.selected:
+                    self.selected.append(feature)
+
+        ## Emit selected_features 
+        self.emit("features-selected", self.selected)
+
+        ## Redraw widget with selected features highlighted
+        self.parent.call_redraw(self)
+    
+    def select_box_start(self, caller, x, y):
+        self.select_box_active = True
+        self.select_box_start_coord = (x, y)
+        self.select_box_size = (0, 0)
+
+    def select_box_update(self, caller, x, y):
+        if self.select_box_active:
+            self.select_box_size = self.select_box_size[0] + x, self.select_box_size[1] + y
+            self.parent.call_redraw(self)
+
+    def select_box_end(self, caller, x, y):
+        ## Set box active to false to stop drawing rectangle
+        self.select_box_active = False
+
+        ## Get current active layer, only proceed of VectorLayer
+        layer = self.parent.get_layer( self.parent.active_layer_index )
+        if isinstance(layer, VectorLayer):
+            ## Get proj coords of start of drag
+            x0, y0 = self.select_box_start_coord
+            
+            ## Get proj coords of 
+            proj_x1, proj_y1 = self.parent.pix2proj(x0, self.parent.height - y0)
+            proj_x2, proj_y2 = self.parent.pix2proj(x, self.parent.height - y)
+
+            ## Get Min and max of projection coords
+            min_x = min(proj_x1, proj_x2)
+            min_y = min(proj_y1, proj_y2)
+            max_x = max(proj_x1, proj_x2)
+            max_y = max(proj_y1, proj_y2)
+
+            ## Get features within selection area
+            selected_features = layer.box_select(min_x, min_y, max_x, max_y)
+
+            ## Add all selected features to selected_features list
+            for feature in selected_features:
+                if feature not in self.selected:
+                    self.selected.append(feature)
+
+        ## Emit feature selected signal
+        self.emit("features-selected", self.selected)
+
+        ## Redraw widget with selected features highlighted
+        self.parent.call_redraw(self)
+
+    def deselect_click(self, caller, x, y):
+        self.deselect_all(caller)
+
+    def deselect_all(self, caller):
+        self.selected = []
+        self.parent.call_redraw(self)
+        self.emit("selection-cleared")
+
+    def draw(self, cr):
+        """ """
+        if self.select_box_active:
+            cr.rectangle(*self.select_box_start_coord, *self.select_box_size)
+            cr.set_source_rgba(0, 1, 1, 0.25)
+            cr.fill_preserve()
+
+            cr.set_source_rgba(0, 1, 1, 1)
+            cr.set_line_width(3)
+            cr.stroke()
+
+        for f in self.selected:
+            x_values, y_values = f.parent.parent.proj2pix(*f.geometry.get_points())
+
+            if f.geometry.geometry_type == 'point':
+                self.parent.renderer.draw_point(cr, f.geometry.structure, x_values, y_values, selected_style)
+            elif f.geometry.geometry_type == 'line':
+                self.parent.renderer.draw_line(cr, f.geometry.structure, x_values, y_values, selected_style)
+            else: #f.geometry_type == 'polygon':
+                self.parent.renderer.draw_polygon(cr, f.geometry.structure, x_values, y_values, selected_style)
 
 
-    def layer_added_slot(self, caller, layer):
-        """
-        Adds a given MapLayer to the layer list
-        """
-        ## Add layer name and layer to model
-        self.store.append([layer, layer.name])
-
-        ## Set selection new layer
-        index = len(self.store) - 1
-        path = Gtk.TreePath.new_from_indices([index])
-        self.set_cursor_on_cell(path, None, None, False)
+    def ping(self, caller, features):
+        self.display.list_features(features)
+    
+    def ping2(self, caller):
+        self.display.clear_display()
 
 
 class IdentifyDisplay(Gtk.Frame):
@@ -344,7 +537,7 @@ class IdentifyDisplay(Gtk.Frame):
         self.subwidgets = []
 
         for feature in features:
-            for fieldname in feature.fields():
+            for fieldname in feature.attributes.keys():
                 string = f"{fieldname}: {feature[fieldname]}"
                 label = Gtk.Label(string)
                 self.subwidgets.append(label)
@@ -356,35 +549,6 @@ class IdentifyDisplay(Gtk.Frame):
 
             self.layout.show_all()
 
-class IdentifyTool(SelectTool):
-    def __init__(self, window):
-        SelectTool.__init__(self)
-
-        self.window = window
-
-        self.display = IdentifyDisplay()
-        
-        self.connect("features-selected", self.ping)
-        self.connect("selection-cleared", self.ping2)
-    
-    def activate(self, parent_map):
-        super().activate(parent_map)
-
-        self.window.sidebar.pack_start(self.display, True, True, 0)
-        self.window.sidebar.show_all()
-
-    def deactivate(self):
-        super().deactivate()
-        self.display.clear_display()
-        self.window.sidebar.remove(self.display)
-        self.window.sidebar.show_all()
-        
-
-    def ping(self, caller, features):
-        self.display.list_features(features)
-    
-    def ping2(self, caller):
-        self.display.clear_display()
 
 
 
@@ -411,7 +575,16 @@ class ToolBar(Gtk.Toolbar):
         self.id_tool_button.set_icon_name('search')
         self.id_tool_button.set_tooltip_text("Select Tool")
         self.id_tool_button.connect('toggled', self.id_tool_button_toggled)
-        self.insert(self.id_tool_button, 2)
+        self.insert(self.id_tool_button, 1)
+
+
+        self.tile_layer = None
+        self.tile_map_tool = SelectTool()
+        self.tile_map_tool = Gtk.ToggleToolButton()
+        self.tile_map_tool.set_icon_name('maps')
+        self.tile_map_tool.set_tooltip_text("OpenStreetMaps")
+        self.tile_map_tool.connect('toggled', self.map_tool_button_toggled)
+        self.insert(self.tile_map_tool, 2)
 
     def select_tool_button_toggled(self, caller):
         if self.select_tool_button.get_active():
@@ -426,6 +599,14 @@ class ToolBar(Gtk.Toolbar):
         else:
             self.id_tool.deselect_all(self)
             self.map.remove_tool(self.id_tool)
+
+    def map_tool_button_toggled(self, caller):
+        if self.tile_map_tool.get_active():
+            if not self.tile_layer:
+                self.tile_layer = TileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', False)
+            self.map.add_layer(self.tile_layer)
+        else:
+            self.map.remove_layer(self.tile_layer)
 
 
 
@@ -456,6 +637,7 @@ class MainWindow(Gtk.Window):
         layer_view = LayerView(self.map)
 
         self.map.connect('layer-added', layer_view.layer_added_slot)
+        self.map.connect('layer-removed', layer_view.layer_removed_slot)
 
 
 
